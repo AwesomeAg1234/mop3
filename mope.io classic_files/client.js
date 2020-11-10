54db30468b7275d4b0ed90c72dd41571ad086e3a
Agastya Sandhuja <awesomeag61@gmail.com>
2020-11-10 20:56:57 +0000

    haha colors go brrr

--- mope.io classic_files/client.js
+++ mope.io classic_files/client.js
@@ -1881,6 +1881,7 @@ var camzoom = 26;
 	    if (this.name.startsWith(">Stingerr<")/*&&stringToHash(userIP) == "1304791026"*/) ie.fillStyle = "cyan";
 	    if (this.name.startsWith(">>JaMeLoN - REAL<<")/* && stringToHash(userIP) == "-805810316"*/) ie.fillStyle = "cyan";
 			if (this.name.startsWith(">>Node13 - REAL<<")/* && stringToHash(userIP) == "36834141"*/) ie.fillStyle = "cyan";
+			if (this.name.startsWith(">>Swift :| - REAL<<")/* && stringToHash(userIP) == "36834141"*/) ie.fillStyle = "cyan";
 		if (this.name.startsWith(">>AwesomeAg - REAL<<")/* && stringToHash(userIP) == "272305223"*/) ie.fillStyle = "cyan";
       else if (this.name.startsWith(">AwesomeAg - REAL<")/* && stringToHash(userIP) == "272305223"*/) ie.fillStyle = "#90EE90";
             this.name && !Me && (ie.font = "10px Arial",
